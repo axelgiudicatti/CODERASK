6e10e332ee6df58baeb1b89573763938e3e733e5
axelgiudicatti <111992058+axelgiudicatti@users.noreply.github.com>
2023-05-16 11:46:42 -0300

    Add files via upload

---
+++ PreEntrega3Giudicatti - PARA CODERASK/js/pay.js
@@ -0,0 +1,132 @@
+/*                                          VARIABLES Y FUNCIONES NECESARIAS. OBJETOS, CONSTRUCTORES, CLASES, METODOS. ARRAYS DE OBJETOS */
+const precioipa = 300;
+const precioirishostout = 275;
+const preciogolden = 250;
+let stockipa = 250;
+let stockirish = 250;
+let stockstout = 250;
+let stockgolden = 250;
+let total = 0;
+let cantidadtotal = 0;
+
+let carritoJS = JSON.parse(localStorage.getItem("birras")) ?? [
+  { cantidad: 0 },
+  { cantidad: 0 },
+  { cantidad: 0 },
+  { cantidad: 0 },
+];
+
+class Cerveza {
+  constructor(estilo, precio, cantidad, stock) {
+    this.estilo = estilo;
+    this.precio = parseFloat(precio);
+    this.cantidad = parseFloat(cantidad);
+    this.subtotal = this.precio * this.cantidad;
+    this.stock = stock;
+  }
+  agregaritem(cantidadaagregar) {
+    this.cantidad = this.cantidad + cantidadaagregar;
+    this.subtotal = this.precio * this.cantidad;
+    this.stock = this.stock - cantidadaagregar;
+  }
+  eliminaritem(cantidadaeliminar) {
+    this.cantidad = this.cantidad - cantidadaeliminar;
+    this.subtotal = this.precio * this.cantidad;
+    this.stock = this.stock + cantidadaeliminar;
+  }
+}
+
+const IPACART = new Cerveza("IPA", precioipa, 0, stockipa);
+const IRISHCART = new Cerveza("IRISH", precioirishostout, 0, stockirish);
+const STOUTCART = new Cerveza("STOUT", precioirishostout, 0, stockstout);
+const GOLDENCART = new Cerveza("GOLDEN", preciogolden, 0, stockgolden);
+
+let carrito = [IPACART, IRISHCART, STOUTCART, GOLDENCART];
+
+for (let ind = 0; ind < 4; ind++) {
+  carrito[ind].agregaritem(carritoJS[ind].cantidad);
+}
+
+function calculartotales() {
+  total = 0;
+  cantidadtotal = 0;
+  carrito.forEach((cart) => {
+    total += cart.subtotal;
+    cantidadtotal += cart.cantidad;
+  });
+}
+
+let pedido = JSON.parse(localStorage.getItem("pedido")) ?? [];
+let idpedido = JSON.parse(localStorage.getItem("numeropedido")) ?? 0;
+let pedidosconfirmados = JSON.parse(localStorage.getItem("confirmados")) ?? [];
+
+const pagocredito = () => {
+  let tarjeta = document.querySelector("#envios");
+  let predatos = [];
+  tarjeta.addEventListener("submit", (e) => {
+    e.preventDefault();
+    idpedido++;
+    predatos.push("Pedido numero " + idpedido);
+    for (x = 0; x < 6; x++) {
+      predatos.push(e.target.children[x].value);
+    }
+    datos = predatos.join();
+    pedidoconf = pedido.concat(datos);
+    pedidosconfirmados.push(pedidoconf);
+    localStorage.setItem("confirmados", JSON.stringify(pedidosconfirmados));
+    pedido = [];
+    datos = [];
+    carrito = [
+      { cantidad: 0 },
+      { cantidad: 0 },
+      { cantidad: 0 },
+      { cantidad: 0 },
+    ];
+    localStorage.setItem("numeropedido", JSON.stringify(idpedido));
+    localStorage.setItem("birras", JSON.stringify(carrito));
+    localStorage.setItem("pedido", JSON.stringify(pedido));
+    window.location.href = "../index.html";
+  });
+};
+
+const pagomp = (x, y) => {
+  let mpzone = document.querySelector(x);
+  mpzone.addEventListener("click", (e) => {
+    e.preventDefault();
+    idpedido++;
+    pedidoconf = pedido;
+    pedidosconfirmados.push(pedidoconf);
+    localStorage.setItem("confirmados", JSON.stringify(pedidosconfirmados));
+    pedido = [];
+    datos = [];
+    carrito = [
+      { cantidad: 0 },
+      { cantidad: 0 },
+      { cantidad: 0 },
+      { cantidad: 0 },
+    ];
+    localStorage.setItem("numeropedido", JSON.stringify(idpedido));
+    localStorage.setItem("birras", JSON.stringify(carrito));
+    localStorage.setItem("pedido", JSON.stringify(pedido));
+    window.location.href = y;
+  });
+};
+
+const totalCarrito = ({ estilo, precio, cantidad }) => {
+  let endorder = document.querySelector("#totalcarrito");
+  endorder.innerHTML =
+    `<h2> El total de tu compra es ` +
+    total +
+    ` y te llevas ` +
+    cantidadtotal +
+    ` latas </h2>`;
+};
+
+/*                                           RUNNER                                        */
+calculartotales();
+carrito.forEach((item) => {
+  totalCarrito(item);
+});
+pagocredito();
+pagomp("#mercadopago", "https://www.mercadopago.com.ar/");
+pagomp("#pagar", "../index.html");
